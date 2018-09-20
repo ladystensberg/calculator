0dc6f553491e4b8624c59016f0ad7acc74c2040c
Sarah Stensberg <ladystensberg@gmail.com>
2018-09-20 15:44:57 -0700

    add some additional UI elements and create basic variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 /*----- constants -----*/
 
+const ZERO = 0;
+const ONE = 1;
+const TWO = 2;
+const THREE = 3;
+const FOUR = 4;
+const FIVE = 5;
+const SIX = 6;
+const SEVEN = 7;
+const EIGHT = 8;
+const NINE = 9;
+
 /*----- app's state (variables) -----*/
+
+var currentNumber;
+var previousNumber;
+var runningTotal;
 
 /*----- cached element references -----*/
 
+var sum = 0; // ADD FUNCTION that if numbers are larger than 13, do something
+var total = document.getElementById("total");
+var clear = document.getElementById("AC");
+var calcContainer = document.querySelector("main");
+total.innerHTML = `<p>${sum}</p>`;
+
 /*----- event listeners -----*/
+
+calcContainer.addEventListener("click", function(event) {
+    console.log(event.target.id);
+});
 
 /*----- functions -----*/
